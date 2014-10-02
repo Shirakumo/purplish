9b16ec55e04b7d378e07899e2db3ef2369d91558
Shinmera <shinmera@tymoon.eu>
2014-10-02 22:54:20 +0200

    Completed API funcs.

--- api.lisp
+++ api.lisp
@@ -6,8 +6,36 @@
 
 (in-package #:org.tymoonnext.radiance.purplish)
 
+(defmacro with-board ((board id) &body body)
+  (let ((bid (gensym "ID")))
+    `(let* ((,bid (or (parse-integer ,id :junk-allowed T)) ,id)
+            (,board (dm:get-one 'purplish-boards (etypecase ,bid
+                                                   (fixnum (db:query (:= '_id ,bid)))
+                                                   (string (db:query (:= 'title ,bid)))))))
+       (unless ,board
+         (error 'api-argument-invalid :argument ',id :message "No such board found."))
+       ,@body)))
+
+(defmacro with-post ((post id) &body body)
+  (let ((pid (gensym "ID")))
+    `(let* ((,pid ,id)
+            (,post (dm:get-one 'purplish-posts (db:query (:= '_id (or (parse-integer ,pid :junk-allowed T) "-1"))))))
+       (unless ,post
+         (error 'api-argument-invalid :argument ',id :message "No such post found."))
+       ,@body)))
+
+(defmacro with-post-accessible ((post-dm) &body body)
+  (let ((post (gensym "POST")))
+    `(let ((,post ,post-dm))
+       (unless (or (user:check (auth:current) '(purplish post change))
+                   (and (= (dm:field ,post "registered") 1)
+                        (string-equal (dm:field ,post "author")
+                                      (user:username (auth:current)))))
+         (error 'api-auth-error :message "You do not have permission to change this post."))
+       ,@body)))
+
 (define-api purplish/boards () ()
-  (api-output (db:select 'purplish-boards (db:query (:= 'visibility "p")))))
+  (api-output (db:select 'purplish-boards (db:query (:= 'visible 1)))))
 
 (define-api purplish/board/threads (board &optional skip amount) ()
   (let ((skip (or (parse-integer skip :junk-allowed T) 0))
@@ -52,25 +80,57 @@
     (api-output posts)))
 
 (define-api purplish/post/files (post) ()
-  (let ((post (dm:get-one 'purplish-posts (db:query (:= '_id (or (parse-integer post :junk-allowed T) "-1"))))))
-    (unless post
-      (error 'api-argument-invalid :argument 'post :message "No such post found."))
+  (with-post (post post)
     (api-output (db:select 'purplish-files (db:query (:= 'post (dm:field post '_id)))))))
 
-(define-api purplish/board/create (title &optional description visibility) (:access (purplish admin boards))
-  )
+(define-api purplish/board/create (name &optional description visible) (:access (purplish board create))
+  (when (dm:get-one 'purplish-boards (db:query (:= 'title title)))
+    (error 'api-argument-invalid :argument 'name :message "A board with that name already exists."))
+  (db:insert 'purplish-boards `((name . ,name)
+                                (description . ,(or description ""))
+                                (visible . ,(if (string= visible "true") 1 0))))
+  (api-output "Board created."))
 
-(define-api purplish/thread/create (title text files &optional author) ()
-  )
+(define-api purplish/thread/create (board title text files &optional author) ()
+  (with-board (board board)
+    (create-post (dm:field board "_id") -1 title text files (or* author (auth:current)))
+    (api-output "Thread created.")))
 
 (define-api purplish/thread/delete (thread) ()
-  )
+  (with-post (thread thread)
+    (unless (= (dm:field thread "parent") -1)
+      (error 'api-argument-invalid :argument 'thread :message "This isn't a thread."))
+    (unless (user:check (auth:current) '(purplish thread delete))
+      (error 'api-auth-error :message "You do not have permission to delete threads."))
+    (dolist (post (dm:get 'purplish-posts (db:query (:= 'parent (dm:field thread "_id")))))
+      (delete-post post))
+    (delete-post thread :recache T)))
 
 (define-api purplish/post/create (thread &optional author title text files) ()
-  )
+  (with-post (thread thread)
+    (unless (= (dm:field post "parent") -1)
+      (error 'api-argument-invalid :argument 'thread :message "This isn't a thread."))
+    (create-post (dm:field thread "board") (dm:field thread "_id") title text files (or* author (auth:current)))
+    (api-output "Post created.")))
 
 (define-api purplish/post/edit (post &optional title text) ()
-  )
+  (with-post (post post)
+    (with-post-accessible (post)
+      (edit-post post title text)
+      (api-output "Post edited."))))
 
-(define-api purplish/post/delete (post) ()
-  )
+(define-api purplish/post/delete (post &optional purge) ()
+  (with-post (post post)
+    (with-post-accessible (post)
+      (unless (/= (dm:field post "parent") -1)
+        (error 'api-argument-invalid :argument 'post :message "This is a thread."))
+      (cond
+        ((and (string= purge "true")
+              (user:check (auth:current) '(purplish post purge)))
+         (delete-post post :recache T :purge T)
+         (api-output "Post purged."))
+        ((string= purge "true")
+         (error 'api-auth-error :message "You do not have permission to purge posts."))
+        (T
+         (delete-post post :recache T)
+         (api-output "Post deleted."))))))
