d3f15f19901c433a364a7c6e93329e88a4e9db15
Shinmera <shinmera@tymoon.eu>
2014-10-04 15:27:41 +0200

    Fixed POST/CREATE files arg and test for content

--- api.lisp
+++ api.lisp
@@ -110,13 +110,15 @@
     (delete-post thread :purge T)
     (api-output "Thread purged.")))
 
-(define-api purplish/post/create (thread &optional author title text files username) ()
+(define-api purplish/post/create (thread &optional author title text files[] username) ()
   (unless (or (not username) (string= username ""))
     (error 'api-argument-invalid :argument 'username :message "Hi, spambot."))
+  (unless (or title text files[])
+    (error 'api-argument-missing :argument 'text :message "Title, text or file required."))
   (with-post (thread thread)
     (unless (= (dm:field thread "parent") -1)
       (error 'api-argument-invalid :argument 'thread :message "This isn't a thread."))
-    (create-post (dm:field thread "board") (dm:field thread "_id") title text files (or* author (auth:current)))
+    (create-post (dm:field thread "board") (dm:field thread "_id") title text files[] (or* author (auth:current)))
     (api-output "Post created.")))
 
 (define-api purplish/post/edit (post &optional title text) ()
