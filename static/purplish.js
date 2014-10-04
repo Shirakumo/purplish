4072fb44958d9312be1a7b1f2dfce1951641f013
Shinmera <shinmera@tymoon.eu>
2014-10-04 21:10:40 +0200

    Basic JS to make multifiles work and show the correct timestamp in the postbox

---
+++ static/purplish.js
@@ -0,0 +1,40 @@
+$(function (){
+    function zeroPadDate(date){
+        return ("0" + date).slice(-2);
+    }
+    
+    function formatDate(date){
+        return date.getFullYear()+"."+
+            zeroPadDate(date.getMonth())+"."+
+            zeroPadDate(date.getDay())+" "+
+            zeroPadDate(date.getHours())+":"+
+            zeroPadDate(date.getMinutes())+":"+
+            zeroPadDate(date.getSeconds());
+    }
+    
+    $("#replybox .time").text(formatDate(new Date()));
+
+    function registerFileRemove(element){
+        element.dblclick(function(){
+            $(this).remove();
+        });
+    }
+
+    function registerFileChange(element){
+        element.change(function(){
+            var li = document.createElement("li");
+            var span = document.createElement("span");
+            var input = document.createElement("input");
+            $(input).attr({"type": "file", "name": "files[]"});
+            $(span).text($(this).val().split(/(\\|\/)/g).pop());
+            $(li).append(span);
+            $(li).append($(this).detach());
+            $("#replybox .files .filelist").append(li);
+            $("#replybox .files").prepend(input);
+            registerFileRemove($(li));
+            registerFileChange($(input));
+        });
+    }
+    
+    registerFileChange($("#replybox .files>input"));
+});
