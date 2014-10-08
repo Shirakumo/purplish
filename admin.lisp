1b02981a7a230e69f43fe4e24262cd2584495ac8
Shinmera <shinmera@tymoon.eu>
2014-10-08 20:04:33 +0200

    Added admin panels for config and headers

--- admin.lisp
+++ admin.lisp
@@ -34,4 +34,41 @@
       (r-clip:process
        T
        :error error :info (when info (format NIL "Recached ~{~a~^, ~}" info))
-       :boards (boards)))))
+       :boards (boards))))
+
+  (admin:define-panel settings purplish (:access (purplish admin conifg) :icon "fa-gears" :tooltip "Change the purplish configuration" :lquery (template "admin-config.ctml"))
+    (with-actions (error info)
+        ((:save
+          (setf (config-tree :purplish :title) (post-var "title")
+                (config-tree :purplish :description) (post-var "description")
+                (config-tree :purplish :news) (post-var "news")
+                (config-tree :purplish :thumb :width) (post-var "thumb-width")
+                (config-tree :purplish :thumb :height) (post-var "thumb-height")
+                (config-tree :purplish :thumb :gif) (post-var "thumb-gif")
+                (config-tree :purplish :file :size-limit) (post-var "file-size-limit"))))
+      (r-clip:process
+       T :error error :info info)))
+
+  (defun header-id (header)
+    (format NIL "~a.~a" (pathname-name header) (pathname-type header)))
+
+  (defun header-src (header)
+    (format NIL "/static/purplish/headers/~a" (header-id header)))
+
+  (admin:define-panel headers purplish (:access (purplish admin headers) :icon "fa-image" :tooltip "Add or remove header images." :lquery (template "admin-headers.ctml"))
+    (with-actions (error info)
+        ((:delete
+          (dolist (file (or (post-var "selected[]") (list (post-var "file"))))
+            (delete-file (merge-pathnames file *headers*)))
+          (setf info "Header deleted."))
+         (:upload
+          (let ((target))
+            (loop do (setf target (merge-pathnames (format NIL "~a.~a"
+                                                           (make-random-string 16)
+                                                           (mimes:mime-file-type (mimes:mime-lookup (second (post-var "file")))))
+                                                   *headers*))
+                  while (probe-file target))
+            (uiop:copy-file (first (post-var "file")) target))
+          (setf info "Header uploaded.")))
+      (r-clip:process
+       T :error error :info info :headers (uiop:directory-files *headers*)))))
