7b6424ea2d59f68f167254872787ef334b179ca3
Shinmera <shinmera@tymoon.eu>
2016-10-30 13:14:45 +0100

    Use r-clip and radiance time functions.

--- cache.lisp
+++ cache.lisp
@@ -183,35 +183,7 @@
   (recache-frontpage)
   (recache-atom))
 
-(defun date-machine (stamp)
-  (when (integerp stamp) (setf stamp (local-time:universal-to-timestamp stamp)))
-  (let ((local-time:*default-timezone* local-time:+utc-zone+))
-    (local-time:format-timestring
-     NIL stamp :format '((:year 4) "-" (:month 2) "-" (:day 2) "T" (:hour 2) ":" (:min 2) ":" (:sec 2)))))
-
-(defun date-human (stamp)
-  (when (integerp stamp) (setf stamp (local-time:universal-to-timestamp stamp)))
-  (let ((local-time:*default-timezone* local-time:+utc-zone+))
-    (local-time:format-timestring
-     NIL stamp :format '((:year 4) "." (:month 2) "." (:day 2) " " (:hour 2) ":" (:min 2) ":" (:sec 2)))))
-
-(defun date-fancy (stamp)
-  (when (integerp stamp) (setf stamp (local-time:universal-to-timestamp stamp)))
-  (let ((local-time:*default-timezone* local-time:+utc-zone+))
-    (local-time:format-timestring
-     NIL stamp :format '(:long-weekday ", " :ordinal-day " of " :long-month " " :year ", " :hour ":" (:min 2) ":" (:sec 2) " UTC"))))
-
 ;; Template helpers
-(lquery:define-lquery-function purplish-time (node time)
-  (let ((stamp (local-time:universal-to-timestamp time)))
-    (setf (plump:attribute node "datetime")
-          (date-machine stamp))
-    (setf (plump:attribute node "title")
-          (date-fancy stamp))
-    (setf (plump:children node) (plump:make-child-array))
-    (plump:make-text-node node (date-human stamp)))
-  node)
-
 (lquery:define-lquery-function purplish-cache (node type object)
   (let ((path (ecase type
                 (:board (board-cache object))
